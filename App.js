11af8509fd97fbd960b5a433a75be14fc1907d52
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2025-01-11 09:29:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ChatBot from './components/Chatbot.js'
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBot>Welcome to Chatbot</ChatBot>
+    </div>
+  );
+}
+
+export default App;
